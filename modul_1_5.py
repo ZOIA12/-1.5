6398c5c61e398b51af04d636260237d4bb423f0b
ZOIA12 <varia2019@gmail.com>
2024-09-09 19:04:51 +0300

    Add files via upload

---
+++ modul_1_5.py
@@ -0,0 +1,7 @@
+immutable_var=([1,2,3], 'house', 0)
+print(immutable_var)
+immutable_var[0][0] = 7
+print(immutable_var)
+mutable_list=[1,2,3, 'house', 0]
+mutable_list[3] = 'skhool'
+print(mutable_list)
